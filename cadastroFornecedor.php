ae67d2863826ddbda82a41f3eb6705e5b666487e
Maria Luiza Zortea <maluzortea13@gmail.com>
2022-09-29 09:07:59 -0300

    fornecedor

--- cadastroFornecedor.php
+++ cadastroFornecedor.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <?php include ('menu.php'); ?>
+
+    
     
     <form action="recebeFornecedor.php" method="POST">
     <div class="container">
@@ -15,12 +17,86 @@
     <form action="dados_fornecedor.html" method="POST">
         <p>
             <strong>Cadastro do fornecedor</strong>
-            <p>Nome completo: </label><br> <input type="text" name="nome"></p>
-            <p>Telefone:</label><br> <input type="text" name="fone"></p>
-            <p>Email: </label><br><input type="text" name="email"></p>
-            <p>Endereço: </label><br><input type="text" name="endereço"></p>
-            <p>Material: </label><br><input type="text" name="material"></p>
-            <p>Observação: </label><br><input type="text" name="comentarios"></p>
+            <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="form-group">
+                        Nome completo:
+                        <input class="form-control" type="text" name="nome">
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="form-group">
+
+                        Telefone:
+                        <input class="form-control" type="text" name="fone" placeholder="(XX) XXXXX-XXXX">
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="form-group">
+
+                        Email:
+                        <input class="form-control" type="text" name="email" placeholder="name@example.com">
+                    </div>
+                </div>
+            </div>
+        </div>
+           
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        Material:
+                        <input class="form-control" type="text" name="material">
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="form-group">
+                        Observação:
+                        <input class="form-control" type="text" name="comentarios">
+                    </div>
+                </div>
+            </div>
+            <h3>Cadastre o endereço do fornecedor</h3>
+        <form action="dados_endereco.html" method="POST">
+                <div class="form-group">
+               Rua:
+                <input class="form-control" type="text" name="rua">
+            </div>
+            <form action="dados_endereco.html" method="POST">
+                <div class="form-group">
+               Bairro:
+                <input class="form-control" type="text" name="bairro">
+            </div>
+            <div class="form-group">
+              Cidade:
+                <input class="form-control" type="text" name="cidade">
+            </div>
+            <div class="form-group">
+              CEP:
+                <input class="form-control" type="text" name="cep">
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="form-group">
+                       Uf:
+                        <select input class="form-control" type="text" name="uf"> 
+                <option value="0">Selecione</option>
+                <option value="SC">SC</option>
+                <option value="RS">RS</option>
+                <option value="PR">PR</option>
+                <option value="outra">Outra</option>
+            </select>
+                    </div>
+                </div>
+           
+                <div class="col-sm-6">
+                    <div class="form-group">
+
+                        Outra:
+                        <input class="form-control" type="text" name="outra">
+                    </div>
+                </div>
             <div class="form-group">
         <input class="btn btn-secondary" type="reset" value="Limpar">
         <input class="btn btn-primary" type="submit" value="Cadastrar">
